577b11d7db8469470eb4c5847da2dba143a61de4
Reine Zeineddine <rhz06@mail.aub.edu>
2025-11-27 21:19:03 +0200

    Delete app directory

--- app/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-from .common import ORMBase, WithTimestamps
-from .doctor import DoctorCreate, DoctorUpdate, DoctorOut
-from .patient import PatientCreate, PatientUpdate, PatientOut
-from .operating_room import OperatingRoomCreate, OperatingRoomUpdate, OperatingRoomOut
-from .surgery import SurgeryCreate, SurgeryUpdate, SurgeryOut
-from .transcription import TranscriptionCreate, TranscriptionUpdate, TranscriptionOut
-from .note import NoteCreate, NoteUpdate, NoteOut
-from .note_analysis import NoteAnalysisCreate, NoteAnalysisUpdate, NoteAnalysisOut
-from .notification import NotificationCreate, NotificationUpdate, NotificationOut
